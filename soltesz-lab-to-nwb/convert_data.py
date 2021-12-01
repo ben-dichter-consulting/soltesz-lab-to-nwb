51e8a16de298667cc5d280b763af91e1a0fa7293
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-12-01 16:00:59 -0500

    debugging ophys

--- soltesz-lab-to-nwb/convert_data.py
+++ soltesz-lab-to-nwb/convert_data.py
@@ -24,7 +24,6 @@ animal_path = os.path.join(base_path, animal)
 nwbfile_path = os.path.join(animal_path, "FullTesting.nwb")
 
 # Point to the various files for the conversion
-speed_file_path = os.path.join(animal_path, "GRIN" + animal[2:] + suffix + ".speed.npy")
 opto_file_path = os.path.join(animal_path, 'GRIN' + animal[2:] + suffix+ '.sbx')
 
 # # Enter Session and Subject information here - uncomment any fields you want to include
@@ -41,9 +40,6 @@ subject_info = dict(
 
 # # Input arguments for each data interface
 source_data = dict(
-    TreadmillData=dict(
-        file_path=str(speed_file_path)
-    ),
     OptophysData=dict(
         file_path=str(opto_file_path)
     )
@@ -52,38 +48,39 @@ source_data = dict(
 # # # Initialize  converter
 converter = DataNWBConverter(source_data=source_data)
 
-# metadata_schema = converter.get_metadata_schema()
+metadata_schema = converter.get_metadata_schema()
 # pprint(metadata_schema, width=300)
 
 # # # Get metadata from source data
 # # # For actual data formats, this generally pulls informatin from the header files for each interface
-metadata = converter.get_metadata()
+#metadata = converter.get_metadata()
 
-# # # User-input metadata
-metadata["NWBFile"].update(session_description=session_description)
-metadata["NWBFile"].update(session_start_time=str(session_start))
-metadata["Subject"] = subject_info
-# these should be linked automatically(?) - validation suggests these should be strings..
-metadata['Ophys']['ImagingPlane'][0]['device'] = metadata['Ophys']['Device'][0]['name']
-metadata['Ophys']['TwoPhotonSeries'][0]['imaging_plane'] = metadata['Ophys']['ImagingPlane'][0]
+
+# # # # User-input metadata
+# metadata["NWBFile"].update(session_description=session_description)
+# metadata["NWBFile"].update(session_start_time=str(session_start))
+# metadata["Subject"] = subject_info
+# # these should be linked automatically(?) - validation suggests these should be strings..
+# metadata['Ophys']['ImagingPlane'][0]['device'] = metadata['Ophys']['Device'][0]['name']
+# metadata['Ophys']['TwoPhotonSeries'][0]['imaging_plane'] = metadata['Ophys']['ImagingPlane'][0]
 
 # conversion_options_schema = converter.get_conversion_options_schema()
 # print("Conversion options for each data interface: \n")
 # pprint(conversion_options_schema["properties"], width=120)
 
 # # # Conversion options for each interface
-conversion_options = dict(
-    TreadmillData=dict(
-        sampling_rate=sampling_rate
-    ),
-    OptophysData=dict()
-)
+# conversion_options = dict(
+#     TreadmillData=dict(
+#         sampling_rate=sampling_rate
+#     ),
+#     OptophysData=dict()
+# )
 
-# # Run conversion
-converter.run_conversion(
-    metadata=metadata,
-    nwbfile_path=str(nwbfile_path),
-    conversion_options=conversion_options,
-    save_to_file=True,  # If False, this instead returns the NWBFile object in memory
-    overwrite=True,  # If False, this appends an existing file
-)
+# # # Run conversion
+# converter.run_conversion(
+#     metadata=metadata,
+#     nwbfile_path=str(nwbfile_path),
+#     conversion_options=conversion_options,
+#     save_to_file=True,  # If False, this instead returns the NWBFile object in memory
+#     overwrite=True,  # If False, this appends an existing file
+# )
